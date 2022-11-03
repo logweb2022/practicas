3f2aa1cfe61b8f7ac7a5df30430221683ead973a
Santos449 <programadorsantos@hotmail.com>
2022-11-03 19:44:08 -0300

    subo practica1

---
+++ practica1.py
@@ -0,0 +1,44 @@
+# print("Hello World")
+# print(3+5)
+# usuario = input("Ingrese usuario ")
+# print("Hola",usuario)
+# nro1 = int(input("Ingrese un número "))
+# nro2 = int(input("Ingrese un número "))
+# suma = (nro1+nro2)
+# print("Resultado de la suma:",suma)
+
+#Comparación entre 2 números
+# nro1 = (input("Ingrese un número "))
+# nro2 = (input("Ingrese un número "))
+
+# if nro1>nro2:
+#     print(nro1,"es el número mayor")
+# elif nro1<nro2:
+#     print(nro2,"es el número mayor")
+# else:
+#     print("Son iguales")
+
+#Comparación entre 3 números
+# a = (input("Ingrese un número "))
+# b = (input("Ingrese un número "))
+# c = (input("Ingrese un número "))
+
+# if a!=b and b!=c and c!=a:
+#     if a>b:
+#         if a>c:
+#             print("El mayor número es: ", a)
+#         else:
+#             print("El mayor número es: ", c)
+
+#     else:
+#         if b>c:
+#             print("El mayor número es: ", b)
+#         else:
+#             print("El mayor número es: ", c)
+            
+# else:
+#     print("Ingrese números diferentes")
+    
+    
+
+
