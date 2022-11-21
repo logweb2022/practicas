4178975110463ac79c1dc600849861e604608b27
Santos449 <programadorsantos@hotmail.com>
2022-11-21 19:25:11 -0300

    29

--- Clase 29. Clases y objetos.practicas.py
+++ Clase 29. Clases y objetos.practicas.py
@@ -1,24 +1,18 @@
 class alumnos:
     def __init__(self, nombre, nota):
-        self.nombre = nombre
-        self.nota = nota
+        self.nombre = input("Ingrese su nombre ")
+        self.nota = int(input("Ingrese su nota "))
 
     def __str__(self):
         return(f'{self.nombre} obtuvo {self.nota} puntos')
 
-    def grabar(self):
-        self.nombre= input("Ingrese nombre")
-        self.nota= input("Ingrese nota")
-
-    def __init__(self):
-        self.nombre = input("Ingrese su nombre")
-        self.nota = input("Ingrese su nota")
+    
 
     def mostrar_estado(self):
         print(f'El Estado de {self.nombre} es {self.nota}')
-        if self.nota <= 4:
+        if self.nota<4:
             print("Desaprobó")
-        elif self.nota <9:
+        elif self.nota<9:
             print("Bueno")
         else:
             print("Excelente")
